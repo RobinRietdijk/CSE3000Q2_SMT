fd765eb46eae13995a436ef47bc047f6b8016c79
Robin Rietdijk <robin@rjrietdijk.com>
2025-12-17 16:41:37 +0100

    Add documentation

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
 def format_elapsed(elapsed: float) -> str:
+    """ Helper function for formatting the elapsed time in ms or s based on value
+
+    Args:
+        elapsed (float): Float of the elapsed time
+
+    Returns:
+        str: Formatted string with either time in ms or s
+    """
     if elapsed < 1.0:
         ms = elapsed*1000
         return f"{ms:.3f} ms"
